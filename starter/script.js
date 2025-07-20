c342dd36a69d255342ea9fe26af2a74344d82e68
irorere juliet <irorerejuliet@gmail.com>
2025-07-20 06:06:56 +0100

    Creating variables

---
+++ starter/script.js
@@ -0,0 +1,24 @@
+let juliet = 'Beautiful';
+console.log(40 + 8 + 23);
+console.log('JUliet');
+console.log(23);
+
+let firstName = "Annable";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let juliet_matilda = "jM"
+let $function = 27;
+
+let person = "Juliet";
+let pI = 3.1415;
+
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = 'pogrammer';
+let job2 = 'teacher';
+
+console.log(myFirstJob)
