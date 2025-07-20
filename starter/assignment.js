5c1d17fbd74e105c90ca260f102fa0c90573370d
irorere juliet <irorerejuliet@gmail.com>
2025-07-20 21:45:11 +0100

    data types, variables and operators

---
+++ starter/assignment.js
@@ -0,0 +1,36 @@
+// let country = 'Nigeria';
+// console.log(country);
+
+// let continent = 'African';
+// console.log(continent);
+
+// let population = 237527782;
+// console.log(population);
+
+// let isIsLand = false;
+// console.log(isIsLand);
+
+// let language 
+// console.log(language);
+
+let nigeria = 118763891;
+nigeria ++;
+console.log(nigeria);
+console.log();
+
+let finLand = 6000000;
+console.log(finLand);
+
+console.log(finLand > nigeria);
+
+finLand = 33000000;
+console.log(nigeria > finLand);
+
+let description = 'Portugal is in Europe, and its 11 million people speak portuguese';
+console.log(description);
+
+
+
+
+
+
--- starter/script.js
+++ starter/script.js
@@ -1,24 +1,77 @@
-let juliet = 'Beautiful';
-console.log(40 + 8 + 23);
-console.log('JUliet');
-console.log(23);
+// let juliet = 'Beautiful';
+// console.log(40 + 8 + 23);
+// console.log('JUliet');
+// console.log(23);
 
-let firstName = "Annable";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Annable";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let juliet_matilda = "jM"
-let $function = 27;
+// // Variable name convention
+// let juliet_matilda = "jM"
+// let $function = 27;
 
-let person = "Juliet";
-let pI = 3.1415;
+// let person = "Juliet";
+// let pI = 3.1415;
 
 
-let myFirstJob = "Programmer";
-let myCurrentJob = "Teacher";
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
 
-let job1 = 'pogrammer';
-let job2 = 'teacher';
+// let job1 = 'pogrammer';
+// let job2 = 'teacher';
 
-console.log(myFirstJob)
+// console.log(myFirstJob)
+
+
+
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// let iMustLearnJavascript = true;
+// console.log(iMustLearnJavascript);
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof 'Anne');
+
+// javascriptIsFun = 'YES!'
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+
+
+///These are math operator
+const now = 2037;
+const ageJuliet = now - 1998;
+const ageSarah = now - 2018;
+console.log(ageJuliet, ageSarah);
+
+console.log(ageJuliet * 2, ageJuliet / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 
+
+const firstName = 'Juliet';
+const  lastName = 'Irorere';
+console.log(firstName + ' ' + lastName);
+
+//These are assignment operators
+let x = 10 + 5; //15
+x += 10; // is the same thimg as x = x + 10 = 25
+x *= 4; //is the same thing as x = x * 4 = 100
+x++;  // x = x + 1
+console.log(x);
+
+
+
+// comparison operator
+console.log(ageJuliet > ageSarah);  // <, >, >=, <=,
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >=18;
+
+console.log(now - 1998 > now - 2018);
+
